0cce3ef2b7b5edc1cf025a14230c42fbe9a6907a
JOEL-8899 <56861930+JOEL-8899@users.noreply.github.com>
2022-12-11 17:08:15 -0500

    maintaining a local state for keeping track of the rating

--- src/star-component/star-component.js
+++ src/star-component/star-component.js
@@ -1,39 +1,37 @@
 import "./startComponent.css"
+import {useState} from "react";
 
 const StarComponent = ({disabled  , rating }) =>{
 
-    const starRatingVal = (clickedStar) =>{
-        // return the corresponding rating given by the user
-        //use state to keep track of data and update accordingly... use
-        rating = clickedStar;
-    }
+    const [currentRating, setRating] = useState(rating);
+
     return(
                 <div className="d-flex align-items-center justify-content-center">
                     <div className="row justify-content-center">
                         <div className="rating-wrapper align-self-center border-5 d-inline-flex pt-2 pe-3 " dir="rtl">
                             <input type="radio" id="5-star-rating"  name="star-rating"
-                                  className="h-100 w-100 d-none" disabled={disabled} onChange={() => starRatingVal(5)}
-                            checked={rating == 5}/>
+                                  className="h-100 w-100 d-none" disabled={disabled} onChange={() => setRating(5)}
+                            checked={currentRating == 5}/>
                                 <label htmlFor="5-star-rating" className="star-rating d-inline-flex  pt-4 ps-2" role="button">
                                     <i className="fas fa-star d-inline-block"  ></i>
                                 </label>
-                                <input type="radio" id="4-star-rating" checked={rating == 4} name="star-rating" disabled={disabled} onChange={() => starRatingVal(4)} className="h-100 w-100 d-none"/>
+                                <input type="radio" id="4-star-rating" checked={currentRating == 4} name="star-rating" disabled={disabled} onChange={() => setRating(4)} className="h-100 w-100 d-none"/>
                                     <label htmlFor="4-star-rating"
                                             className="star-rating star  d-inline-flex  pt-4 ps-2" role="button">
                                         <i className="fas fa-star d-inline-block"  ></i>
                                     </label>
                                     <input type="radio" id="3-star-rating"
-                                           checked={rating == 3} disabled={disabled} onChange={() => starRatingVal(3)} name="star-rating"  className="h-100 w-100 d-none"/>
+                                           checked={currentRating == 3} disabled={disabled} onChange={() => setRating(3)} name="star-rating"  className="h-100 w-100 d-none"/>
                                         <label htmlFor="3-star-rating" className="star-rating star  d-inline-flex  pt-4 ps-2" role="button">
                                             <i className="fas fa-star d-inline-block" ></i>
                                         </label>
                                         <input type="radio" id="2-star-rating"
-                                               checked={rating == 2} disabled={disabled} onChange={() => starRatingVal(2)} name="star-rating"  className="h-100 w-100 d-none"/>
+                                               checked={currentRating == 2} disabled={disabled} onChange={() => setRating(2)} name="star-rating"  className="h-100 w-100 d-none"/>
                                             <label htmlFor="2-star-rating" className="star-rating star  d-inline-flex  pt-4 ps-2" role="button">
                                                 <i className="fas fa-star d-inline-block" ></i>
                                             </label>
                                             <input type="radio" id="1-star-rating"
-                                                   checked={rating == 1} disabled={disabled} onChange={() => starRatingVal(1)} name="star-rating"  className="h-100 w-100 d-none"/>
+                                                   checked={currentRating == 1} disabled={disabled} onChange={() => setRating(1)} name="star-rating"  className="h-100 w-100 d-none"/>
                                                     <label htmlFor="1-star-rating" className="star-rating star d-inline-flex pt-4 ps-2"
                                                        role="button">
                                                     <i className="fas fa-star d-inline-block" ></i>
